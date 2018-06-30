d89364583a906d97b7c115b72915843054d130e6
JoyZero <painforbighead@gmail.com>
2018-06-30 21:38:48 +0800

    Commit before leaving again...

--- src/DataAnalysis/CSVDataProcessor.java
+++ src/DataAnalysis/CSVDataProcessor.java
@@ -1,5 +1,34 @@
 package DataAnalysis;
 
-public class CSVDataProcessor {
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 
+import gitDataProcess.Util;
+
+public class CSVDataProcessor {
+	public static final String dirPath = "E:/data/dataset/tera/";
+	
+	public static List<String> getVerFileList(String name) {
+		List<String> files = new LinkedList<String>();
+		String arffDirPath = dirPath + name + "/";
+		File dir = new File(arffDirPath);
+		if (!dir.exists() || !dir.isDirectory()) {
+			System.out.println("directory error!");
+			return files;
+		}
+		for (File subFile : dir.listFiles()) {
+			String fileName = subFile.getName();
+			if (fileName.endsWith(".csv")) {
+				files.add(fileName);
+			}
+		}
+		Util.sortFileName(files);
+		return files;
+	}
+	
+	public static void filterData(String filename) {
+		
+	}
+	
 }
